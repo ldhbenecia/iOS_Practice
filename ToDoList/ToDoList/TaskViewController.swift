98fce9aea3756b02b9bbe14d07d0db09f0775a47
DongHyeok Lim <zx07050@naver.com>
2021-10-05 22:10:33 +0900

    initial commit

---
+++ ToDoList/ToDoList/TaskViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TaskViewController.swift
+//  ToDoList
+//
+//  Created by 임동혁 on 2021/10/04.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask() {
+        
+//        let newCount = count - 1
+//
+//        UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+    }
+
+}
